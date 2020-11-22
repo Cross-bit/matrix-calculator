b07fcdf93b64eeb9503fc4c5340ddba8cf69a00b
Cross-bit <ondra.kryz@gmail.com>
2020-11-23 00:28:03 +0100

    úprava sum

--- matrixCalc/operations/matrix_sub.py
+++ matrixCalc/operations/matrix_sub.py
@@ -7,13 +7,13 @@ class MatrixSum:
         self.mx2 = mx1
         self.product = Matrix(mx1.n, mx2.m)
 
-    def sum(self, substract = False):
+    def sum(self):
         if not (check_if_matrix_dims_are_same(self.mx1, self.mx2)):
             print("Zadané matice nejsou stejných roměrů!")
             return
 
         for i in range(self.mx1.m):
             for j in range(self.mx2.n):
-                self.product = self.mx1[i][j] + (self.mx2[i][j] * (-1 if substract else 1))
+                self.product = self.mx1[i][j] + self.mx2[i][j]
 
         return self.product
